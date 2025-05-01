12e7b8d2649878103b0c144d44a1ef4e506d23bc
chapinkj <chapinkj@gmail.com>
2025-05-01 15:49:46 -0400

    updated css

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-function closeAllAccordions() {
-    accordions.forEach((accordion) => {
-        if (accordion.classList.contains('show')) {
-            const bsCollapse = new bootstrap.Collapse(accordion, {
-                toggle: false
-            });
-            bsCollapse.hide();
-        }
-    });
-}
-document.addEventListener('DOMContentLoaded', function() {
-    const accordions = document.querySelectorAll('.accordion-collapse');
-    document.querySelectorAll('.top-link').forEach((topLink) => {
-        topLink.addEventListener('click', closeAllAccordions);                
-    });
-    
-    anyOpenAccordion(accordions);
-});
+  function redirectHash() {
+    if (location.hash === "#sample-data-visualizations") {
+      location.hash = "#data";
+    }
+  }
+  window.addEventListener("load", redirectHash);
+  window.addEventListener("hashchange", redirectHash);
+
