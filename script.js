c8f56d9f34c3f1dcd287d55a22e00e7cfb6f51c7
chapinkj <chapinkj@gmail.com>
2025-05-12 17:00:07 -0400

    css update, removed js

--- script.js
+++
@@ -1,8 +0,0 @@
-  function redirectHash() {
-    if (location.hash === "#sample-data-visualizations") {
-      location.hash = "#data";
-    }
-  }
-  window.addEventListener("load", redirectHash);
-  window.addEventListener("hashchange", redirectHash);
-
